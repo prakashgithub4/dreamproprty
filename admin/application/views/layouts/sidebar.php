a939ed62b95d097dc56685fdaf6b11babadccf8d
prakashgithub4 <prakashgithub4@gmail.com>
2020-07-15 14:16:12 -0700

    admin sidebar set up

--- admin/application/views/layouts/sidebar.php
+++ admin/application/views/layouts/sidebar.php
@@ -16,89 +16,98 @@
                                 <a href="<?php echo base_url(); ?>dashbord" class="active"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
                             </li>
                             <li>
-                                <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
+                                <a href="#"><i class="fa fa-pencil"></i> Manage Blog<span class="fa arrow"></span></a>
                                 <ul class="nav nav-second-level">
-                                    <li>
+                                    <!-- <li>
                                         <a href="flot.html">Flot Charts</a>
-                                    </li>
+                                    </li> -->
                                     <li>
-                                        <a href="morris.html">Morris.js Charts</a>
+                                        <a href="morris.html">Blog</a>
                                     </li>
                                 </ul>
                                 <!-- /.nav-second-level -->
-                            </li>
-                            <li>
-                                <a href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
-                            </li>
-                            <li>
-                                <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
+							</li> 
+							
+							<li>
+                                <a href="#"><i class="fa fa-male"></i> Manage Agets<span class="fa arrow"></span></a>
                                 <ul class="nav nav-second-level">
+                                    <!-- <li>
+                                        <a href="flot.html">Flot Charts</a>
+                                    </li> -->
                                     <li>
-                                        <a href="panels-wells.html">Panels and Wells</a>
-                                    </li>
-                                    <li>
-                                        <a href="buttons.html">Buttons</a>
-                                    </li>
-                                    <li>
-                                        <a href="notifications.html">Notifications</a>
-                                    </li>
-                                    <li>
-                                        <a href="typography.html">Typography</a>
-                                    </li>
-                                    <li>
-                                        <a href="icons.html"> Icons</a>
-                                    </li>
-                                    <li>
-                                        <a href="grid.html">Grid</a>
+                                        <a href="#">Agent</a>
                                     </li>
                                 </ul>
                                 <!-- /.nav-second-level -->
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
+							</li>
+							<li>
+                                <a href="#"><i class="fa  fa-bed"></i> Manage Property<span class="fa arrow"></span></a>
                                 <ul class="nav nav-second-level">
+                                    <!-- <li>
+                                        <a href="flot.html">Flot Charts</a>
+                                    </li> -->
                                     <li>
-                                        <a href="#">Second Level Item</a>
+                                        <a href="morris.html">Properties</a>
+									</li>
+									<li>
+                                        <a href="morris.html">Total Properties</a>
                                     </li>
-                                    <li>
-                                        <a href="#">Second Level Item</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Third Level <span class="fa arrow"></span></a>
-                                        <ul class="nav nav-third-level">
-                                            <li>
-                                                <a href="#">Third Level Item</a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Third Level Item</a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Third Level Item</a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Third Level Item</a>
-                                            </li>
-                                        </ul>
-                                        <!-- /.nav-third-level -->
-                                    </li>
+									
                                 </ul>
                                 <!-- /.nav-second-level -->
                             </li>
-                            <li>
-                                <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
+
+							<li>
+                                <a href="#"><i class="fa fa-credit-card-alt"></i> Manage Offers<span class="fa arrow"></span></a>
                                 <ul class="nav nav-second-level">
+                                    <!-- <li>
+                                        <a href="flot.html">Flot Charts</a>
+                                    </li> -->
                                     <li>
-                                        <a href="blank.html">Blank Page</a>
-                                    </li>
-                                    <li>
-                                        <a href="login.html">Login Page</a>
+                                        <a href="morris.html">Offers</a>
                                     </li>
                                 </ul>
                                 <!-- /.nav-second-level -->
                             </li>
+							<li>
+                                <a href="#"><i class="fa fa-building"></i> Manage Contact<span class="fa arrow"></span></a>
+                                <ul class="nav nav-second-level">
+                                    <!-- <li>
+                                        <a href="flot.html">Flot Charts</a>
+                                    </li> -->
+                                    <li>
+                                        <a href="#">Contact</a>
+                                    </li>
+                                </ul>
+                                <!-- /.nav-second-level -->
+							</li>	
+							<li>
+                                <a href="#"><i class="fa fa-truck"></i> Manage Service<span class="fa arrow"></span></a>
+                                <ul class="nav nav-second-level">
+                                    <!-- <li>
+                                        <a href="flot.html">Flot Charts</a>
+                                    </li> -->
+                                    <li>
+                                        <a href="#">Service</a>
+                                    </li>
+                                </ul>
+                                <!-- /.nav-second-level -->
+							</li>
+							
+							<li>
+                                <a href="#"><i class="fa fa-google-plus"></i> Social Managements<span class="fa arrow"></span></a>
+                                <ul class="nav nav-second-level">
+                                    <!-- <li>
+                                        <a href="flot.html">Flot Charts</a>
+                                    </li> -->
+                                    <li>
+                                        <a href="#">Social</a>
+                                    </li>
+                                </ul>
+                                <!-- /.nav-second-level -->
+                            </li>
+							
+
                         </ul>
                     </div>
                 </div>
